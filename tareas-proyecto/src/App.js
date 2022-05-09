5c51eb3862d404a8fbf9cb45d594559c0a419a5f
KaiserSov <sara310105@gmail.com>
2022-05-09 18:05:34 -0500

    documentación ---El código Front fue documentado con éxito

--- tareas-proyecto/src/App.js
+++ tareas-proyecto/src/App.js
@@ -1,13 +1,28 @@
-import { useEffect, useState } from "react";
+/**
+ * A continuación veremos un crud encargado de elaborar listas y tareas
+ * @author: Sara Oquendo Valle
+ * Este proyecto de listas fue elaborado con una base del coach Raúl
+ */
+
+
+import React, { Fragment, useEffect, useState } from "react";
 import { Container, Col, Row } from "reactstrap";
 import ListaTareas from "./Components/ListaTareas";
 import ListaPrincipal from "./Components/ListaPrincipal";
 import axios from 'axios';
 import FormularioTarea from "./Components/FormularioTarea";
 
+/**
+ * 
+ * @returns devuelve los componentes, el formulario  de tareas dentro de la lista principal, para que el usuario pueda crear varias listas.
+ */
 function App() {
-    const [listas, setListas] = useState([]);
 
+    const [listas, setListas, setTareas, tareas] = useState([]);
+
+    /**
+     * Encargado de traer la lista principal
+     */
     const cargarLista = () =>{
         axios.get('http://localhost:8080/tareas')
         .then(({data}) => setListas(data))
@@ -15,18 +30,29 @@ function App() {
 
     useEffect(cargarLista, []);
 
+    /**
+     * Encargado de guardar los valores cargados anteriormente
+     * @param {*} values 
+     */
     const onSubmitL = (values) => {
         axios.post("http://localhost:8080/tareas", values)
         .then(() => cargarLista());
     }
 
-    const eliminarLista = (tarea) => {
+    /**
+     * Elimina una lista seleccionada
+     * @param {*} listasllama al contenido en la lista
+     */
+    const eliminarLista = (listas) => {
         axios.delete("http://localhost:8080/tareas/${tarea.id}")
         .then(() => cargarLista());
     }
 
 
 
+    /**
+     * Encargado de llamar el formulario de tareas
+     */
     const cargarTareas = () =>{
         axios.get('http://localhost:8080/tareas')
         .then(({data}) => setTareas(data))
@@ -34,19 +60,26 @@ function App() {
 
     useEffect(cargarTareas, []);
 
+    /**
+     * Encargado de guardar los valores cargados anteriormente
+     * @param {*} values 
+     */
     const onSubmit = (values) => {
         axios.post("http://localhost:8080/tareas", values)
         .then(() => cargarTareas());
     }
 
+    /**
+     * Elimina una tarea completada o que ya no se desee realizar
+     * @param {*} tarea llama al contenido en la lista
+     */
     const eliminarTarea = (tarea) => {
         axios.delete("http://localhost:8080/tareas/${tarea.id}")
         .then(() => cargarTareas());
     }
 
     return (
-        <>
-        <Container>
+        <Fragment>
         <Row>
                 <Col md={6}>
                   <ListaPrincipal 
@@ -57,7 +90,8 @@ function App() {
                   <ListaPrincipal onSubmitL={onSubmitL}/>
                 </Col>
             </Row>
-
+            
+           
         <Container>
             <Row>
                 <Col md={6}>
@@ -70,8 +104,9 @@ function App() {
                 </Col>
             </Row>
         </Container>
-        </Container>
-        </>
+        
+        </Fragment>
+        
     );
 }
 
